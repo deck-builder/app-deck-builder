a088fb15c6d491822f60046f370ac553b4d3b427
tchesa <cesarolimpio@gmail.com>
2021-07-08 23:51:05 -0300

    first style assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './style/index.scss';
+import './assets/style/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
